8effd9f3df40b6b92a2f05e4d54750b624afc4a7
Andreas Müller <am@0x7.ch>
2018-05-02 00:25:16 +0200

    typo fix

--- nodemcu-mode.el
+++ nodemcu-mode.el
@@ -24,7 +24,7 @@
 ;;; Commentary:
 
 ;; nodemcu-mode provides a wrapper around nodemcu-tool and
-;; nodemcu-wrapper, so that NodeMCU devices can be programmed and used
+;; nodemcu-uploader, so that NodeMCU devices can be programmed and used
 ;; from within Emacs.
 
 ;;; Code:
